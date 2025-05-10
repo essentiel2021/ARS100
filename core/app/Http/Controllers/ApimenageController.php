b693808cca0ff66b0902e8edc225d9c4a66e28a4
Angui <anguijathi@gmail.com>
2025-05-10 19:16:06 +0000

    updateApimenage

--- core/app/Http/Controllers/ApimenageController.php
+++ core/app/Http/Controllers/ApimenageController.php
@@ -66,92 +66,64 @@ class ApimenageController extends Controller
             $menage = new Menage();
            
         }
-        if ($menage->producteur_id != $request->producteur_id) {
-            $hasMenage = Menage::where('producteur_id', $request->producteur_id)->exists();
-            if ($hasMenage) {
-                return response()->json("Ce producteur a déjà un menage enregistré", 501);
-            }
-        }
+        // if ($menage->producteur_id != $request->producteur_id) {
+        //     $hasMenage = Menage::where('producteur_id', $request->producteur_id)->exists();
+        //     if ($hasMenage) {
+        //         return response()->json("Ce producteur a déjà un menage enregistré", 501);
+        //     }
+        // }
+       
         $menage->producteur_id  = $request->producteur_id;
-        $menage->quartier  = $request->quartier;
-        $menage->ageEnfant0A5  = $request->ageEnfant0A5;
-        $menage->ageEnfant6A17  = $request->ageEnfant6A17;
-        $menage->enfantscolarises  = $request->enfantscolarises;
-        $menage->enfantsPasExtrait = $request->enfantsPasExtrait;
-        $menage->enfantsPasExtrait6A17 = $request->enfantsPasExtrait6A17;
-        $menage->boisChauffe     = $request->boisChauffe;
-        $menage->separationMenage = $request->separationMenage;
-        $menage->eauxToillette    = $request->eauxToillette;
-        $menage->eauxVaisselle    = $request->eauxVaisselle;
-        $menage->wc    = $request->wc;
-        $menage->sources_eaux    = $request->sources_eaux;
-        $menage->type_machines    = $request->type_machines;
-        $menage->garde_machines    = $request->garde_machines;
-        $menage->equipements    = $request->equipements;
-        $menage->traitementChamps    = $request->traitementChamps;
-        $menage->nomApplicateur   = $request->nomApplicateur;
-        $menage->numeroApplicateur   = $request->numeroApplicateur;
-        $menage->activiteFemme    = $request->activiteFemme;
-        $menage->nomActiviteFemme    = $request->nomActiviteFemme;
-        $menage->champFemme    = $request->champFemme;
-        $menage->nombreHectareFemme    = $request->nombreHectareFemme;
-        $menage->autreMachine    = $request->autreMachine;
-        $menage->autreEndroit    = $request->autreEndroit;
-        $menage->userid =  $request->userid;
-        $menage->autreSourceEau   = $request->autreSourceEau;
-        $menage->etatAutreMachine   = $request->etatAutreMachine;
-        $menage->etatatomiseur   = $request->etatatomiseur;
-        $menage->etatEpi  = $request->etatEpi;
-        $menage->typeActivite = $request->typeActivite;
-        $menage->nomActiviteAgricole = $request->nomActiviteAgricole;
-        $menage->autreActiviteAgricole = $request->autreActiviteAgricole;
-        $menage->nomActiviteNonAgricole = $request->nomActiviteNonAgricole;
-        $menage->autreActiviteNonAgricole = $request->autreActiviteNonAgricole;
-        $menage->capitalDemarrage = $request->capitalDemarrage;
-        $menage->formation = $request->formation;
-        $menage->dureeActivite = $request->dureeActivite;
-        $menage->autreCapital = $request->autreCapital;
-        $menage->nombreHectareConjoint = $request->nombreHectareConjoint;
-        $menage->entite = $request->entite;
+        $menage->nom = $request->nom;
+        $menage->prenoms = $request->prenoms;
+        $menage->sexe = $request->sexe;
+        $menage->statutMatrimonial = $request->statutMatrimonial;
+        $menage->autre_lien_parente = $request->autre_lien_parente;
+        $menage->dateNaiss = $request->dateNaiss;
+        $menage->phone1 = $request->phone1;
+        $menage->niveau_etude = $request->niveau_etude;
+        $menage->autre_instruction = $request->autre_instruction;
+        $menage->autre_instruction = $request->statut_scolaire;
+        $menage->categorie_ethnique    = $request->categorie_ethnique;
         //dd(json_encode($request->all()));
 
         $menage->save();
-        if ($menage != null) {
-            $id = $menage->id;
-            $datas  = $data2 = [];
-            if (($request->sourcesEnergie != null)) {
-                Menage_sourceEnergie::where('menage_id', $id)->delete();
-                $i = 0;
-                foreach ($request->sourcesEnergie as $sourceEnergie) {
-                    if (!empty($sourceEnergie)) {
-                        $datas[] = [
-                            'menage_id' => $id,
-                            'source_energie' => $sourceEnergie,
-                        ];
-                    }
+        // if ($menage != null) {
+        //     $id = $menage->id;
+        //     $datas  = $data2 = [];
+        //     if (($request->sourcesEnergie != null)) {
+        //         Menage_sourceEnergie::where('menage_id', $id)->delete();
+        //         $i = 0;
+        //         foreach ($request->sourcesEnergie as $sourceEnergie) {
+        //             if (!empty($sourceEnergie)) {
+        //                 $datas[] = [
+        //                     'menage_id' => $id,
+        //                     'source_energie' => $sourceEnergie,
+        //                 ];
+        //             }
 
-                    $i++;
-                }
-            }
-            if (($request->ordureMenagere != null)) {
-                Menage_ordure::where('menage_id', $id)->delete();
-                foreach ($request->ordureMenagere as $data) {
-                    //dd($ordureMenagere);
+        //             $i++;
+        //         }
+        //     }
+        //     if (($request->ordureMenagere != null)) {
+        //         Menage_ordure::where('menage_id', $id)->delete();
+        //         foreach ($request->ordureMenagere as $data) {
+        //             //dd($ordureMenagere);
 
-                    $data2[] = [
-                        'menage_id' => $id,
-                        'ordure_menagere' => $data,
-                    ];
-                }
-            }
+        //             $data2[] = [
+        //                 'menage_id' => $id,
+        //                 'ordure_menagere' => $data,
+        //             ];
+        //         }
+        //     }
 
-            Menage_sourceEnergie::insert($datas);
-            Menage_ordure::insert($data2);
-        }
+        //     Menage_sourceEnergie::insert($datas);
+        //     Menage_ordure::insert($data2);
+        // }
 
-        if ($menage == null) {
-            return response()->json("Le ménage n'a pas été enregistré", 501);
-        }
+        // if ($menage == null) {
+        //     return response()->json("Le ménage n'a pas été enregistré", 501);
+        // }
 
         return response()->json($menage, 201);
     }
